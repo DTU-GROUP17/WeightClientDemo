6d9477bb0f151d8e05800db61033e7d5cf55b123
Ali.Ghodrat <ght.ali@hotmail.com>
2017-03-13 00:48:07 +0100

    Implementing server socket

--- WeightClientDemo/src/server/Server.java
+++ WeightClientDemo/src/server/Server.java
@@ -2,7 +2,27 @@ package server;
 
 import java.io.*;
 import java.net.*;
-
+//This class implement server socket.
 public class Server {
+	
+	//data input stream defined.
+    private Socket socket = null;
+    private ServerSocket serverS = null;
+    private DataInputStream dataStreamIn = null;
+    
+    //return the local address of server socket and return the port number.
+    public Server(int port){
+    	try {
+            System.out.println("Opening port:" + port );
+            serverS = new ServerSocket(port);
+            System.out.println("Server started on" + serverS.getInetAddress() + "on port" + 
+            serverS.getLocalPort());
+            System.out.println("wait for a client");
+            socket = serverS.accept();
+            
+            }
+    
+    	}
+    
 
 }
