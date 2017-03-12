fa881e1bbb20b93b0b100c262e03c9878194997f
Ali.Ghodrat <ght.ali@hotmail.com>
2017-03-12 19:58:07 +0100

    Server Socket
    Method to implement server socket and exit the program if server couldn't start.

--- WeightClientDemo/src/Client.java
+++ WeightClientDemo/src/Client.java
@@ -13,17 +13,27 @@ import java.util.Scanner;
  */
 public class Client {
 
-	private ServerSocket serSocket;
+	private ServerSocket serverSocket;
 	private static int portNumber = 8000;
 	//private  client;
 	private Scanner scan;
 	private boolean runCheck = true;
-	
+
+/*This method implements server sockets*/
 	public Client(String port){
-		
-		portNumber = Integer.parseInt(posrt)
+
+		portNumber = Integer.parseInt(port);
+
+		try {
+			serverSocket = new ServerSocket(portNumber);
+			scan = new Scanner(System.in);
+		} catch (IOException e) {
+			System.out.println("Error 12: Server could not start. Exit");
+			System.exit(0);
+
+		}
+
 	}
-	
 }
 
 
